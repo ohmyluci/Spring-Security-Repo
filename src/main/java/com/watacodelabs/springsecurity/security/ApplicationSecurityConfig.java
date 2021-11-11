ea1dafbb34431fff8c9cbee55eaf06d889c0c4da
lucidiovacas <lucidiovacas@gmail.com>
2021-11-11 16:18:52 +0100

    Added Pass encoder, users and restrictions for requests

--- src/main/java/com/watacodelabs/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/watacodelabs/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,23 +1,59 @@
 package com.watacodelabs.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/css/*", "js/*")
-                .permitAll()
+                .antMatchers("/", "/css/*", "js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails lucidioUser = User.builder()
+                .username("lucidio")
+                .password(passwordEncoder.encode("pass"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                lucidioUser,
+                adminUser
+        );
     }
 }
